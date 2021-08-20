5c325bca891004ece353b2c995c184467462d055
qedk <1994constant@gmail.com>
2021-08-21 01:03:55 +0530

    Complete auth flow for Twitter

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ require('dotenv').config()
 const fastify = require('fastify')({ logger: true })
 // const oauthPlugin = require('fastify-oauth2')
 const grant = require('grant').fastify()
+const mongoose = require('mongoose')
+const { User } = require('./models/user.js')
 
 fastify
   .register(require('fastify-cookie'))
-  .register(require('fastify-session'), { secret: process.env.COOKIE_KEY, cookie: { secure: false } })
+  .register(require('fastify-session'),
+    {
+      secret: process.env.COOKIE_KEY,
+      cookie: { secure: false, maxAge: 604800 }
+    })
   .register(grant({
     defaults: {
-      origin: 'http://localhost:3000',
+      origin: process.env.BASE_URL,
       transport: 'session',
       state: true,
       prefix: '/login'
@@ -23,7 +29,7 @@ fastify
   }))
 
 fastify.register(require('fastify-cors'), {
-  origin: 'http://localhost:3000',
+  origin: process.env.BASE_URL,
   credentials: true
 })
 
@@ -32,12 +38,34 @@ fastify.get('/', async (request, reply) => {
 })
 
 fastify.get('/login/twitter/done', async (request, reply) => {
-  console.log(request.session.grant.response)
+  const twitterResponse = request.session.grant.response
+  const userQuery = await User.findOne(
+    { "twitter.id": twitterResponse.raw.user_id },
+    { __v:0, createdAt: 0, updatedAt: 0 })
+    .lean()
+  if (!userQuery) {
+    const newUser = new User({
+      twitter: {
+        id: twitterResponse.raw.user_id,
+        screen_name: twitterResponse.raw.screen_name,
+        verified: twitterResponse.profile.verified
+      }
+    })
+    const userObj = await newUser.save()
+    request.session.user = { id: userObj._id }
+  } else {
+    request.session.user = { id: userQuery._id }
+  }
   reply.send()
 })
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log('⌛️ DB connection established...')
     await fastify.listen(process.env.PORT || 3000, function (err, address) {
       if (err) {
         throw err
