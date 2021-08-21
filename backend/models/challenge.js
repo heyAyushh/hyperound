d83cd9bbcf52184ae6e0471630ea63c4b779c5b9
qedk <1994constant@gmail.com>
2021-08-21 23:23:34 +0530

    Add model for challenges

---
+++ backend/models/challenge.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const challengeSchema = new mongoose.Schema({
+  address: {
+    type: String,
+    unique: true,
+  },
+  challenge: {
+    type: String,
+    unique: true
+  },
+  updatedAt: {
+    type: Date,
+    expires: 300,
+    default: Date.now
+  }
+})
+
+module.exports = { Challenge: new mongoose.model('Challenge', challengeSchema) }
