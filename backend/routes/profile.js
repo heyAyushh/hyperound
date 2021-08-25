6d21befcf3201f85475036beffda6328f8143fa1
qedk <1994constant@gmail.com>
2021-08-26 00:34:33 +0530

    Add follow and unfollow endpoints

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 const { User } = require('../models/user.js')
 
 module.exports = function (fastify, opts, done) {
@@ -61,14 +60,112 @@ module.exports = function (fastify, opts, done) {
     }
   }, async (request, reply) => {
     try {
-      const userQuery = await User.findOne(
-        { _id: mongoose.Types.ObjectId(request.params.userId) },
+      const userQuery = await User.findById(request.params.userId,
         { __v: 0, 'followers.users': 0, 'following.users': 0 })
         .lean()
       if (!userQuery) {
         reply.code(404).send()
       } else {
         reply.send(userQuery)
+      }
+    } catch (err) {
+      fastify.log.error('❎ error:' + err)
+      if (!reply.sent) {
+        reply.code(400).send()
+      }
+    }
+  })
+
+  fastify.post('/follow/:userId', {
+    preValidation: [fastify.authenticate],
+    schema: {
+      description: 'Follow a profile by user ID as a particular user',
+      params: {
+        type: 'object',
+        required: ['userId'],
+        properties: {
+          userId: { type: 'string' }
+        }
+      },
+      response: {
+        200: {
+          description: 'Successful response',
+          type: 'object'
+        },
+        404: {
+          description: 'Profile does not exist',
+          type: 'object'
+        }
+      }
+    }
+  }, async (request, reply) => {
+    try {
+      const userQuery = await User.findById(request.params.userId, { followers: 1 }).lean()
+      console.log(userQuery, !userQuery.followers.users.includes(String(request.session.user_id)))
+      if (!userQuery) {
+        reply.code(404).send()
+        return
+      } else {
+        if (!userQuery.followers.users.includes(String(request.session.user_id))) {
+          await User.findByIdAndUpdate(request.params.userId, {
+            $inc: { 'followers.count': 1 },
+            $push: { 'followers.users': request.session.user_id }
+          })
+          await User.findByIdAndUpdate(request.session.user_id, {
+            $inc: { 'following.count': 1 },
+            $push: { 'following.users': request.params.userId }
+          })
+        }
+        reply.send()
+      }
+    } catch (err) {
+      fastify.log.error('❎ error:' + err)
+      if (!reply.sent) {
+        reply.code(400).send()
+      }
+    }
+  })
+
+  fastify.delete('/follow/:userId', {
+    preValidation: [fastify.authenticate],
+    schema: {
+      description: 'Unfollow a profile by user ID as a particular user',
+      params: {
+        type: 'object',
+        required: ['userId'],
+        properties: {
+          userId: { type: 'string' }
+        }
+      },
+      response: {
+        200: {
+          description: 'Successful response',
+          type: 'object'
+        },
+        404: {
+          description: 'Profile does not exist',
+          type: 'object'
+        }
+      }
+    }
+  }, async (request, reply) => {
+    try {
+      const userQuery = await User.findById(request.params.userId, { followers: 1 }).lean()
+      if (!userQuery) {
+        reply.code(404).send()
+        return
+      } else {
+        if (userQuery.followers.users.includes(String(request.session.user_id))) {
+          await User.findByIdAndUpdate(request.params.userId, {
+            $inc: { 'followers.count': -1 },
+            $pull: { 'followers.users': request.session.user_id }
+          })
+          await User.findByIdAndUpdate(request.session.user_id, {
+            $inc: { 'following.count': -1 },
+            $pull: { 'following.users': request.params.userId }
+          })
+        }
+        reply.send()
       }
     } catch (err) {
       fastify.log.error('❎ error:' + err)
