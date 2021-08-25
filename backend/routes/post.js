f75ab600bd787872a4089c69ea2f804fb5d006fd
qedk <1994constant@gmail.com>
2021-08-25 11:44:07 +0530

    Add new favorite endpoint

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -32,14 +32,63 @@ module.exports = function (fastify, opts, done) {
       if (request.body.creator !== request.session.user_id) {
         reply.code(403).send()
       }
-      await Post.create({
+      const newPost = await Post.create({
         creator: mongoose.Types.ObjectId(request.body.creator),
         text: request.body.text,
         content: request.body.content,
         contentType: request.body.contentType,
         locked: request.body.locked
       })
-      reply.code(201).send()
+      reply.code(201).send({ _id: newPost._id })
+    } catch (err) {
+      fastify.log.error('❎ error:' + err)
+      if (!reply.sent) {
+        reply.code(400).send()
+      }
+    }
+  })
+
+  fastify.patch('/favorite', {
+    preValidation: [fastify.authenticate],
+    schema: {
+      description: 'Toggles the favourite state on a post for a particular user',
+      body: {
+        type: 'object',
+        required: ['id'],
+        properties: {
+          id: { type: 'string' }
+        }
+      },
+      response: {
+        200: {
+          description: 'Successful response',
+          type: 'object'
+        }
+      }
+    }
+  }, async (request, reply) => {
+    try {
+      const post = await Post.findOne({ _id: request.body.id })
+      if (!post) {
+        reply.code(404).send()
+      } else {
+        if (post.favorites.users.includes(request.session.user_id)) {
+          await Post.updateOne(
+            { _id: request.body.id },
+            {
+              $inc: { 'favorites.count': -1 },
+              $pull: { 'favorites.users': request.session.user_id }
+            })
+        } else {
+          await Post.updateOne(
+            { _id: request.body.id },
+            {
+              $inc: { 'favorites.count': 1 },
+              $push: { 'favorites.users': request.session.user_id }
+            })
+        }
+      }
+      reply.send()
     } catch (err) {
       fastify.log.error('❎ error:' + err)
       if (!reply.sent) {
